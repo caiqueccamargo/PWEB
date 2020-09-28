84fc00a2260c365d2db31684443daae3eb6e3a60
caiqueccamargo <40776917+caiqueccamargo@users.noreply.github.com>
2020-09-28 19:35:36 -0300

    Parte 2

--- Atividade 5/script.js
+++ Atividade 5/script.js
@@ -7,3 +7,24 @@ function calculoMedia()
   media = (n1 + n2 + n3) / 3 ;
   alert("Média das notas: " + media + "");
 }
+
+function calculos()
+{
+  var soma;
+  var sub;
+  var div;
+  var mult;
+  var a1 = parseInt(document.getElementById('a1').value);
+  var a2 = parseInt(document.getElementById('a2').value);
+
+  soma = a1 + a2;
+  sub = a1 - a2;
+  div = a1 / a2;
+  mult = a1 * a2;
+
+  alert("Soma: " + soma + "");
+  alert("Sub: " + sub + "");
+  alert("Multiplicação: " + mult + "");
+  alert("Divisão: " + div + "");
+
+}
