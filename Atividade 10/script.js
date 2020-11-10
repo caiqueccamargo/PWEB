1c5265ea2cbc1c932b97df2505e4e768ce89b1e6
caiqueccamargo <40776917+caiqueccamargo@users.noreply.github.com>
2020-11-09 22:18:41 -0300

    Atividade 10
    Janelas

---
+++ Atividade 10/script.js
@@ -0,0 +1,13 @@
+function mouseOver(){
+    document.getElementById("img1").src = "https://image.freepik.com/vetores-gratis/janela-aberta-marrom_88088-659.jpg"
+
+}
+
+function mouseOut(){
+    document.getElementById("img1").src = "https://image.freepik.com/vetores-gratis/uma-janela-fechada-com-vista-para-a-lagoa_1308-39678.jpg"
+
+}
+
+function destroy(){
+    document.getElementById("img1").src = "http://www.blogdofabossi.com.br/wp-content/uploads/2013/11/janelas.jpg"
+}
