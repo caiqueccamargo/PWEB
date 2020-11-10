b7c13a5ee176a2702763342cef66d372c4248d0f
caiqueccamargo <40776917+caiqueccamargo@users.noreply.github.com>
2020-11-09 23:20:12 -0300

    Atividade 11
    Maiúscula e Minúscula

---
+++ Atividade 11/script.js
@@ -0,0 +1,14 @@
+var min;
+var mai;
+
+
+function minuscula(){
+    min = document.getElementById("text1");
+    min.value = min.value.toLowerCase();
+}
+
+function maiuscula(){
+    mai = document.getElementById("text1");
+    mai.value = mai.value.toUpperCase();
+}
+
